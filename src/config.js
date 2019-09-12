3f1cb17bf20a2e19126bbb134620f22261e099d4
Santiago Casar <santiago@nowhereprod.com>
2019-09-12 13:09:16 -0700

    added localhost url CORS

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  CLIENT_ORIGIN: 'https://conscioussheep-app.now.sh/'
+  CLIENT_ORIGIN: 'https://conscioussheep-app.santiago1.now.sh/' || 'http://localhost:3000/'
 }
