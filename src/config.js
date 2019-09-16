324456548f16de59010c93d0a6cd6b2148487dc9
Santiago Casar <santiago@nowhereprod.com>
2019-09-16 17:46:34 -0400

    working out CORS requirements with webhosting

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   CLIENT_ORIGIN: 'https://conscioussheep-app.santiago1.now.sh',
-  CLIENT_ORIGINAL:'http://localhost:3001'
+  CLIENT_ORIGINAL:'http://localhost:3002'
 }
