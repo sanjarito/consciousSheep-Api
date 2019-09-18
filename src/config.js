d133ef52f4c795c8e8edae9dd0b354d69dc9676d
Santiago Casar <santiago@nowhereprod.com>
2019-09-18 14:58:46 -0400

    changed DB_URL in config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://santiagocasar@localhost/conscioussheep',
+  DB_URL: process.env.DATABASE_URL || 'postgresql://santiagocasar@localhost/conscioussheep',
   CLIENT_ORIGIN: 'https://conscioussheep-app.santiago1.now.sh',
   CLIENT_ORIGINAL:'http://localhost:3002'
 }
