2856603294d287c98d29551e7cebfcc7ca304521
Santiago Casar <santiago@nowhereprod.com>
2019-09-17 22:53:19 -0400

    added DB with Users and Favors tables, also added migration files and seed scripts, using PG and Knex

---
+++ seeds/seed.conscioussheep_users.sql
@@ -0,0 +1,6 @@
+INSERT INTO conscioussheep_users (user_email,user_password,user_zipcode,user_about,user_favortokens)
+VALUES
+  ('sanjarito@hotmail.com', 'password1',45120,'I am a big boy',3),
+  ('hellogoodbye@gmail.com', 'password1',35120,'I am a small alien',2),
+  ('user2@gmail.com', 'password1',12312,'I am GOKU',2),
+  ('user3@gmail.com', 'password1',11120,'I am a superheroe',1)
